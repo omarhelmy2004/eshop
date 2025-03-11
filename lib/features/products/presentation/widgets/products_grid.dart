81af6221d93810e40ffdc95fb260a3e2007ddb2c
omarhelmy2004 <omarhelmy2004@hotmail.com>
2025-03-11 22:09:36 +0200

    product widget part 1

--- lib/features/products/presentation/widgets/products_grid.dart
+++ lib/features/products/presentation/widgets/products_grid.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 
 class ProductsGrid extends StatelessWidget {
@@ -17,12 +16,81 @@ class ProductsGrid extends StatelessWidget {
           mainAxisSpacing: 20,
         ),
         itemBuilder: (context, index) {
-          return Container(
-            height: 20,
-            width: 20,
-            color: Colors.grey,
-          );
+          return ProductCard();
         },
+      ),
+    );
+  }
+}
+
+class ProductCard extends StatefulWidget {
+  const ProductCard({super.key});
+
+  @override
+  _ProductCardState createState() => _ProductCardState();
+}
+
+class _ProductCardState extends State<ProductCard> {
+  bool isWishlisted = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.lightBlueAccent, // Changed background color
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Stack(
+            children: [
+              Container(
+                height: 100,
+                decoration: BoxDecoration(
+                  color: Colors.transparent, // Made background transparent
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Center(
+                  child: Image.network(
+                    'https://m.media-amazon.com/images/I/81uiWMk9dnL._AC_SL1500_.jpg', // Changed to network image
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              Positioned(
+                top: 8,
+                right: 8,
+                child: GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      isWishlisted = !isWishlisted;
+                    });
+                  },
+                  child: Icon(
+                    isWishlisted ? Icons.favorite : Icons.favorite_border,
+                    color: isWishlisted ? Colors.red : Colors.black,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              'Nike air jordan retro fa...',
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+              overflow: TextOverflow.ellipsis, // Added ellipsis for overflow
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+            child: Text(
+              '\$126.00',
+              style: TextStyle(fontSize: 18, color: Colors.black),
+            ),
+          ),
+        ],
       ),
     );
   }
