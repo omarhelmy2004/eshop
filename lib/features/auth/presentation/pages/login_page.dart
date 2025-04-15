c98538841e138f95d631045b2bb27d7cff0be756
omarhelmy2004 <omarhelmy2004@hotmail.com>
2025-04-15 04:27:54 +0200

    login page done

--- lib/features/auth/presentation/pages/login_page.dart
+++ lib/features/auth/presentation/pages/login_page.dart
@@ -1,90 +1,134 @@
 import 'package:eshop/core/presentation/widgets/logo.dart';
+import 'package:eshop/core/presentation/widgets/main_view.dart';
+import 'package:eshop/features/auth/presentation/cubits/login_cubit/login_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import '../widgets/email_input_field.dart';
 import '../widgets/password_input_field.dart';
 import '../widgets/sign_button.dart';
 import 'signup_page.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
 
   static String id = 'LoginPage';
 
   @override
-  Widget build(BuildContext context) {
-    final _formKey = GlobalKey<FormState>();
+  _LoginPageState createState() => _LoginPageState();
+}
 
+class _LoginPageState extends State<LoginPage> {
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       resizeToAvoidBottomInset: true,
-      body: SingleChildScrollView(
-        padding: const EdgeInsets.symmetric(horizontal: 24.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const SizedBox(height: 60),
-              const Logo(),
-              const SizedBox(height: 30),
-              const Text("Login", style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold)),
-              const SizedBox(height: 5),
-              Row(
+      body: BlocConsumer<LoginCubit, LoginState>(
+        listener: (context, state) {
+          if (state is LoginLoading) {
+            // Show a loading indicator
+            showDialog(
+              context: context,
+              barrierDismissible: false,
+              builder: (context) => const Center(child: CircularProgressIndicator()),
+            );
+          } else if (state is LoginSuccess) {
+            // Navigate to the main view or show success message
+            Navigator.pop(context); // Close the loading dialog
+            Navigator.pushReplacementNamed(context, MainView.id, arguments: state.userId);
+          } else if (state is LoginFailure) {
+            // Show an error message
+            Navigator.pop(context); // Close the loading dialog
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(content: Text(state.error)),
+            );
+          }
+        },
+        builder: (context, state) {
+          return SingleChildScrollView(
+            padding: const EdgeInsets.symmetric(horizontal: 24.0),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  const Text("Don't have an account? ", style: TextStyle(fontSize: 18)),
-                  GestureDetector(
-                    onTap: () {
-                      Navigator.pushReplacement(
-                        context,
-                        PageRouteBuilder(
-                          pageBuilder: (context, animation1, animation2) => const SignupPage(),
-                          transitionDuration: Duration.zero,
-                          reverseTransitionDuration: Duration.zero,
+                  const SizedBox(height: 60),
+                  const Logo(),
+                  const SizedBox(height: 30),
+                  const Text("Login",
+                      style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold)),
+                  const SizedBox(height: 5),
+                  Row(
+                    children: [
+                      const Text("Don't have an account? ",
+                          style: TextStyle(fontSize: 18)),
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.pushReplacement(
+                            context,
+                            PageRouteBuilder(
+                              pageBuilder: (context, animation1, animation2) =>
+                                  const SignupPage(),
+                              transitionDuration: Duration.zero,
+                              reverseTransitionDuration: Duration.zero,
+                            ),
+                          );
+                        },
+                        child: const Text(
+                          "Signup",
+                          style: TextStyle(
+                              color: Colors.green,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18),
                         ),
-                      );
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 20),
+                  EmailInputField(
+                    controller: _emailController, // Use TextEditingController
+                    enColor: Colors.green,
+                    foColor: Colors.black,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter your email';
+                      }
+                      return null;
                     },
-                    child: const Text(
-                      "Signup",
-                      style: TextStyle(color: Colors.green, fontWeight: FontWeight.bold, fontSize: 18),
-                    ),
+                  ),
+                  const SizedBox(height: 15),
+                  PasswordInputField(
+                    controller: _passwordController, // Add TextEditingController
+                    enColor: Colors.green,
+                    foColor: Colors.black,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter your password';
+                      }
+                      return null;
+                    },
+                  ),
+                  const SizedBox(height: 25),
+                  SignupButton(
+                    text: 'Login',
+                    formKey: _formKey,
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        final String email = _emailController.text;
+                        final String password = _passwordController.text;
+                        BlocProvider.of<LoginCubit>(context)
+                            .loginUser(email: email, password: password);
+                      }
+                    },
                   ),
                 ],
               ),
-              const SizedBox(height: 20),
-              EmailInputField(
-                enColor: Colors.green,
-                foColor: Colors.black,
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter your email';
-                  }
-                  return null;
-                },
-              ),
-              const SizedBox(height: 15),
-              PasswordInputField(
-                enColor: Colors.green,
-                foColor: Colors.black,
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter your password';
-                  }
-                  return null;
-                },
-              ),
-              const SizedBox(height: 25),
-              SignupButton(
-                text: 'Login',
-                formKey: _formKey,
-                onPressed: () {
-                  if (_formKey.currentState!.validate()) {
-                    // Handle login logic here
-                  }
-                },
-                
-              ),
-            ],
-          ),
-        ),
+            ),
+          );
+        },
       ),
     );
   }
